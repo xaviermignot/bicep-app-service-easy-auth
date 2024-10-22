76d5d11f53a2ea45550d77ea46d2cdb926a6e1c1
Xavier Mignot <code@xmi.fr>
2024-10-22 16:54:54 +0200

    feat: Add parameter for location in deploy script

--- infra/deploy.sh
+++ infra/deploy.sh
@@ -1,7 +1,20 @@
 #!/bin/sh
 
+while getopts "l:" opt; do
+    case "${opt}" in
+    l) location=${OPTARG} ;;
+    \?) echo "Invalid option: -$OPTARG" >&2 || exit 1 ;;
+    esac
+done
+
+if [ -z "$location" ]; then
+  location="francecentral"
+fi
+
 app_id=$(az ad app list --filter "uniqueName eq 'app-app-service-easy-auth'" --query '[].id' -o tsv)
 generate_app_password="$([ "$app_id" ] && echo 'false' || echo 'true')"
 
-script_dir=$(dirname $0)
-az deployment sub create -n deploy-app-service-easy-auth -f "$script_dir/main.bicep" -p location=francecentral -p generateAppRegistrationPassword="$generate_app_password" -l francecentral
+script_dir=$(dirname "$0")
+az deployment sub create -n deploy-app-service-easy-auth -f "$script_dir/main.bicep" \
+  -p location="$location" -p generateAppRegistrationPassword="$generate_app_password" \
+  -l "$location"
